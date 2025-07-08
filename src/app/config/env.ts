3440f77919602eff51529c6c45d20da2f944820e
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-07-08 22:30:40 +0600

    setup done

---
+++ src/app/config/env.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface ENVConfig {
+  PORT: string;
+  DB_URL: string;
+  NODE_ENV: "development" | "production";
+}
+
+const loadEnvVariables = (): ENVConfig => {
+  const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV"];
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing require environment variable ${key}`);
+    }
+  });
+  return {
+    PORT: process.env.PORT as string,
+    DB_URL: process.env.DB_URL as string,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
+  };
+};
+
+export const envVars = loadEnvVariables();
--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,16 @@
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/config/env";
 
 let server: Server;
 const startServer = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/");
+    await mongoose.connect(envVars.DB_URL);
 
     console.log("Connected to DB!!");
-    server = app.listen(5000, () => {
-      console.log("Server is listening to port 5000");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is listening to port ${envVars.PORT}`);
     });
   } catch (error) {
     console.log(error);
