92dc0a78b7adb4745276d0c6b434a6ea438f57a3
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-07-09 22:37:23 +0600

    added apperror handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { envVars } from "../config/env";
 import { Request, Response, NextFunction } from "express";
+import AppError from "../error helpers/AppError";
 
 export const globalErrorHandler = (
   err: any,
@@ -9,8 +10,16 @@ export const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
-  const statusCode = 500;
-  const message = `Something Went Wrong!! ${err.message}`;
+  let statusCode = 500;
+  let message = `Something Went Wrong!! ${err.message}`;
+
+  if (err instanceof AppError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  } else if (err instanceof Error) {
+    statusCode = 500;
+    message = err.message;
+  }
 
   res.status(statusCode).json({
     success: false,
