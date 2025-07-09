cb0f8fe4589dfc02f2db162808877a40e0e2fd7d
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-07-09 22:14:04 +0600

    added service in user

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { UserServices } from "./user.service";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = await UserServices.createUser(req.body);
+
+    res.status(httpStatus.CREATED).json({
+      message: "User Created Successfully",
+      user,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: `Something Went Wrong!! ${error.message}`,
+    });
+  }
+};
+
+export const UserControllers = {
+  createUser,
+};
