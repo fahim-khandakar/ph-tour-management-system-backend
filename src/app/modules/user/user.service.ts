2c73a36160ab28fc8357f009f046e6628bde87c5
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-07-13 00:36:46 +0600

    added many thing

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,11 +1,30 @@
-import { IUser } from "./user.interface";
+import AppError from "../../error helpers/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import httpStatus from "http-status-codes";
+import bcrypt from "bcrypt";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { name, email } = payload;
+  const { email, password, ...rest } = payload;
+
+  const isUserExist = await User.findOne({ email });
+
+  if (isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User Already Exist");
+  }
+
+  const hashedPassword = await bcrypt.hash(password as string, 10);
+
+  const authProvider: IAuthProvider = {
+    provider: "credentials",
+    providerId: email as string,
+  };
+
   const user = await User.create({
-    name,
     email,
+    password: hashedPassword,
+    auths: [authProvider],
+    ...rest,
   });
 
   return user;
