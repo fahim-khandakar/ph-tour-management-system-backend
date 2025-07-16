908d01cd580d761ebb1ab0370667e2704319bfaa
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-07-16 22:10:49 +0600

    passport locally setup done

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,42 +1,89 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { JwtPayload } from "jsonwebtoken";
+import passport from "passport";
 import { envVars } from "../../config/env";
+
+import { setAuthCookie } from "../../utils/setCookie";
 import { createUserTokens } from "../../utils/userTokens";
 import { AuthServices } from "./auth.service";
 import catchAsync from "../../utils/catchAsync";
-import sendResponse from "../../utils/sendResponse";
 import AppError from "../../error helpers/AppError";
-import { setAuthCookie } from "../../utils/setCookie";
+import sendResponse from "../../utils/sendResponse";
 
 const credentialsLogin = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const loginInfo = await AuthServices.credentialsLogin(req.body);
+    // const loginInfo = await AuthServices.credentialsLogin(req.body)
 
-    setAuthCookie(res, loginInfo);
+    passport.authenticate("local", async (err: any, user: any, info: any) => {
+      if (err) {
+        // ❌❌❌❌❌
+        // throw new AppError(401, "Some error")
+        // next(err)
+        // return new AppError(401, err)
 
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: "User Logged In Successfully",
-      data: loginInfo,
-    });
+        // ✅✅✅✅
+        // return next(err)
+        // console.log("from err");
+        return next(new AppError(401, err));
+      }
+
+      if (!user) {
+        // console.log("from !user");
+        // return new AppError(401, info.message)
+        return next(new AppError(401, info.message));
+      }
+
+      const userTokens = await createUserTokens(user);
+
+      // delete user.toObject().password
+
+      const { password: pass, ...rest } = user.toObject();
+
+      setAuthCookie(res, userTokens);
+
+      sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User Logged In Successfully",
+        data: {
+          accessToken: userTokens.accessToken,
+          refreshToken: userTokens.refreshToken,
+          user: rest,
+        },
+      });
+    })(req, res, next);
+
+    // res.cookie("accessToken", loginInfo.accessToken, {
+    //     httpOnly: true,
+    //     secure: false
+    // })
+
+    // res.cookie("refreshToken", loginInfo.refreshToken, {
+    //     httpOnly: true,
+    //     secure: false,
+    // })
   }
 );
 const getNewAccessToken = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken;
-    console.log("refreshToken", refreshToken);
     if (!refreshToken) {
       throw new AppError(
         httpStatus.BAD_REQUEST,
-        "No refresh token received from cookies"
+        "No refresh token recieved from cookies"
       );
     }
     const tokenInfo = await AuthServices.getNewAccessToken(
       refreshToken as string
     );
+
+    // res.cookie("accessToken", tokenInfo.accessToken, {
+    //     httpOnly: true,
+    //     secure: false
+    // })
 
     setAuthCookie(res, tokenInfo);
 
@@ -97,6 +144,7 @@ const googleCallbackController = catchAsync(
       redirectTo = redirectTo.slice(1);
     }
 
+    // /booking => booking , => "/" => ""
     const user = req.user;
 
     if (!user) {
@@ -106,6 +154,13 @@ const googleCallbackController = catchAsync(
     const tokenInfo = createUserTokens(user);
 
     setAuthCookie(res, tokenInfo);
+
+    // sendResponse(res, {
+    //     success: true,
+    //     statusCode: httpStatus.OK,
+    //     message: "Password Changed Successfully",
+    //     data: null,
+    // })
 
     res.redirect(`${envVars.FRONTEND_URL}/${redirectTo}`);
   }
