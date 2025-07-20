5b78e97761cd2adf8722fff06fdecdb9bcfdcc75
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-07-21 00:45:18 +0600

    added manythin

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -14,7 +14,7 @@ export const checkAuth =
       const accessToken = req.headers.authorization;
 
       if (!accessToken) {
-        throw new AppError(403, "No Token Recieved");
+        throw new AppError(403, "No Token Received");
       }
 
       const verifiedToken = verifyToken(
