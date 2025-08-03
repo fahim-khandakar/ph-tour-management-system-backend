56ff3e9da6bcafd303bcd3524ac1eb970b7efa74
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-08-04 00:56:00 +0600

    redis config done

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import app from "./app";
 import { envVars } from "./app/config/env";
 import { seedSuperAdmin } from "./app/utils/seedSuperAdmin";
+import { connectRedis } from "./app/config/redis.config";
 
 let server: Server;
 const startServer = async () => {
@@ -19,6 +20,7 @@ const startServer = async () => {
   }
 };
 (async () => {
+  connectRedis();
   startServer();
   seedSuperAdmin();
 })();
